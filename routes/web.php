3e4fd54d65bb231d22fe4bc06d34ba20811722a0
Alex <nonaname1993@gmail.com>
2018-12-19 19:14:59 +0200

    product controller create new left/destroy

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,17 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@index')->name('admin');
 Route::get('/admin/products', 'ProductsController@index')->name('products');
 Route::get('/admin/product/{id}', 'ProductsController@edit')->name('product');
-Route::post('/admin/product/{id}', 'ProductsController@update')->name('edit.product');
+Route::get('/admin/product/create', 'ProductsController@create')->name('create.product');
+Route::post('/admin/product', 'ProductsController@store')->name('store.product');
+Route::put('/admin/product/{id}', 'ProductsController@update')->name('edit.product');
+
+
+
+Route::delete('/admin/product/{id}', 'ProductsController@destroy')->name('delete.product');
+
+//Route::resource('products', 'ProductsController');
+
+
 //Route::get('/admin/users', 'AdminController@users')->name('users');
 //Route::get('/admin/user/{id}', 'AdminController@user')->name('user');
 //Route::get('/admin/{any}', 'AdminController@index')->where('any', '.*');
